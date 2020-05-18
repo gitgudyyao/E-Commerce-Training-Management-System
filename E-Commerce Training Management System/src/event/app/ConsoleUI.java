eb2d53f0edc67e241776c861c744d3c2792f1809
YY-GitHub <52448621+gitgudyyao@users.noreply.github.com>
2020-05-18 12:05:08 +0800

    Update ConsoleUI.java

--- E-Commerce Training Management System/src/event/app/ConsoleUI.java
+++ E-Commerce Training Management System/src/event/app/ConsoleUI.java
@@ -3,6 +3,9 @@ package event.app;
 import event.domain.*;
 
 import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class ConsoleUI {
@@ -20,10 +23,10 @@ public class ConsoleUI {
 	
 	//UI
 	public void start() {
-		int choice;
-		
+		String choice;
+		boolean true_input;	
 		do {
-		System.out.println("E-Commerce Training Managament System:");
+			System.out.println("E-Commerce Training Managament System:");
 	    	System.out.println("1. Create a new event record");
 	    	System.out.println("2. Add a talk for an event");
 	    	System.out.println("3. Add a guest to invite for an event");
@@ -34,39 +37,76 @@ public class ConsoleUI {
 	    	System.out.println("8. Exit");
 	    	
 	    	System.out.print("Enter your choice (1-8): ");
-	    	choice = scanner.nextInt();
-	
-	    	while (choice < 1 || choice > 8) {
-	        	System.out.println("Invalid choice.");
-	        	System.out.print("Enter your choice (1-8): ");
-	        	choice = scanner.nextInt();
-	    	}
+	    	choice = scanner.nextLine();
+	    	true_input = true;
 	    	//Basic Selection
-	    	switch(choice) {
-	    		case 1: addEvent(); break;
-	    		case 2: addTalk(); break;
-	    		case 3: addGuest(); break;
-	    		case 4: updateGuestStatus(); break;
-	    		case 5: displayAllEvent(); 
-	    		case 6: displayTalks(); break;
-	    		case 7: displayGuests(); break;
-	    		case 8: break;
+	    	switch(choice) 
+	    	{
+	    		case "1": addEvent(); break;
+	    		case "2": addTalk(); break;
+	    		case "3": addGuest(); break;
+	    		case "4": updateGuestStatus(); break;
+	    		case "5": displayAllEvent(); break;
+	    		case "6": displayTalks(); break;
+	    		case "7": displayGuests(); break;
+	    		case "8": break;
+	    		default: true_input = false;
 	    	}
-	
-	    	System.out.println();
-		} while (choice != 8);
+	    	if(true_input == false)
+	    	{
+	    		System.out.println("You have entered an invalid input.");
+	    		System.out.println();
+	    	}
+		}while(choice != "8" || choice !="7" || choice !="6" || choice !="5" || choice != "4" || choice != "3" || choice != "2" || choice != "1");
+    	System.out.println();   	
 	}
 	
-	public void addEvent() {
+	public void addEvent() 
+	{
 		System.out.println("Please enter event title: ");
-		String skip = scanner.nextLine();
-		String theTitle = scanner.nextLine();
+		String theTitle = scanner.nextLine();		
+		if(theTitle.equals("") || theTitle.equals(" "))
+		{
+			while(theTitle.equals("") || theTitle.equals(" "))
+			{
+				System.out.println("Please enter the event title again!");
+				System.out.println("Empty input is not allowed.");
+				theTitle = scanner.nextLine();
+			}
+		}
 
-		System.out.println("Please enter event date: ");
-		String date = scanner.nextLine();
+		System.out.println("Please enter event date in the format of 'DD/MM/YYYY': ");
+		String date = scanner.nextLine();		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/202y");	
+		
+		boolean wrong = true;
+		do {
+			try 
+			{
+				Date d = sdf.parse(date);
+				wrong = false;
+			} 
+			
+			catch (ParseException e) 
+			{
+				System.out.println("Please enter again your date in the format of 'DD/MM/YYYY'.");
+				System.out.println("Enter date that on or later than year 2020.");
+				date = scanner.nextLine();
+			}
+		}while(wrong == true);
+		
 		
 		System.out.println("Please enter event venue: ");
-		String venue = scanner.nextLine();
+		String venue = scanner.nextLine();		
+		if(venue.equals("") || venue.equals(" "))
+		{
+			while(venue.equals("") || venue.equals(" "))
+			{
+				System.out.println("Please enter the event venue again!");
+				System.out.println("Empty input is not allowed.");
+				venue = scanner.nextLine();
+			}
+		}
 
 		System.out.println("Please enter event theme: ");
 		String theme = scanner.nextLine();
@@ -80,31 +120,74 @@ public class ConsoleUI {
 		Event anEvent = selectEvent();
 		if(anEvent != null) {
 			System.out.println("Please enter talk's title: ");
-			String skip = scanner.nextLine();
 			String title = scanner.nextLine();
+			if(title.equals("") || title.equals(" "))
+			{
+				while(title.equals("") || title.equals(" "))
+				{
+					System.out.println("Please enter the talk's title again!");
+					System.out.println("Empty input is not allowed.");
+					title = scanner.nextLine();
+				}
+			}
 			
 			System.out.println("Please enter talk's speaker: ");
 			String speaker = scanner.nextLine();
+			if(speaker.equals("") || speaker.equals(" "))
+			{
+				while(speaker.equals("") || speaker.equals(" "))
+				{
+					System.out.println("Please enter the talk's speaker again!");
+					System.out.println("Empty input is not allowed.");
+					speaker = scanner.nextLine();
+				}
+			}
 			
 			System.out.println("Please enter talk's duration: ");
-			int duration = scanner.nextInt();
-			skip = scanner.nextLine();
+			String duration = scanner.nextLine();
 			
-			controller.addTalk(anEvent, title, speaker,duration);
-		}
+			boolean wrong = true;
+			do
+			{
+				try
+				{
+					int number = Integer.parseInt(duration);
+					wrong = false;
+				}
+				
+				catch(Exception e)
+				{
+					System.out.println("Please enter again the duration correctly.");
+					duration = scanner.nextLine();
+				}
+			}while(wrong == true);
+			
+			controller.addTalk(anEvent, title, speaker, duration);
+			System.out.println("Update successfully!");
+			System.out.println();	
+		}	
 	}
 
 	public void addGuest() {
 		Event anEvent = selectEvent();
 		if(anEvent != null) {
 			System.out.println("Please enter guest's name: ");
-			String skip = scanner.nextLine();
 			String name = scanner.nextLine();
+			if(name.equals("") || name.equals(" "))
+			{
+				while(name.equals("") || name.equals(" "))
+				{
+					System.out.println("Please enter the guest's name again!");
+					System.out.println("Empty input is not allowed.");
+					name = scanner.nextLine();
+				}
+			}
 			
 			System.out.println("Please enter guest's contact number: ");
 			String contact = scanner.nextLine();
 			
 			controller.addGuest(anEvent, name, contact);
+			System.out.println("Guest added succesfully.");
 		}
 	}
 
@@ -113,11 +196,11 @@ public class ConsoleUI {
 		
 		if(aGuest != null) {
 			System.out.println("Please enter the latest status(accepted/rejected): ");
-			String skip = scanner.nextLine();
 			String status = scanner.nextLine();
 			
 			while (!"accepted".equalsIgnoreCase(status) && !"rejected".equalsIgnoreCase(status)) {
-	        	System.out.println("Invalid status.");
+				System.out.println();
+				System.out.println("Invalid status.");
 	        	System.out.print("Please enter the latest status(accepted/rejected): ");
 	        	status = scanner.nextLine();
 	    	}
@@ -131,58 +214,67 @@ public class ConsoleUI {
 	
 	public void displayAllEvent() {
 		int count = controller.getNumberOfEvent();
+		List<Event> events = controller.getAllEvent();
 		
-		if(count > 0) {
-			List<Event> events = controller.getAllEvent();
-			
-			for(int i = 0;i < count;i++) {
+		if(count >= 1) 
+		{
+			for(int i = 0 ; i < count ; i++) 
+			{
 				System.out.println(i+1 + ". " + events.get(i).getTitle());
 			}
-		}else {
+		}
+		else 
+		{
 			System.out.println("Error. No event found.");
 		}
 	}
 	
 	//Returns Event to fit into updateGuestStatus method
-	public Event displayGuests() {
+	public Event displayGuests() 
+	{
 		Event anEvent = selectEvent();
 		
-		if(anEvent!= null) {
-			String skip = scanner.nextLine();
-			
+		if(anEvent!= null) 
+		{		
 			int count = controller.getNumberOfGuest(anEvent);
-			if(count > 0) {
-				List<Guest> guests = controller.getAllGuest(anEvent);
-				
-				for(int i = 0;i < count;i++) {
+			if(count > 0) 
+			{
+				List<Guest> guests = controller.getAllGuest(anEvent);				
+				for(int i = 0;i < count;i++) 
+				{
 					System.out.println(i+1 + ". " + guests.get(i).getName() + "\t Contact: " + guests.get(i).getContact() + "\t Status: " + guests.get(i).getReplyStatus());
 				}
-			}else {
-				System.out.println("No guest is found in this event.");
 			}
-			
-			System.out.println();
+			else 
+			{
+				System.out.println("No guest is found in this event.");
+			}			
 			return anEvent;
-		}else {
+		}
+		else 
+		{
 			return null;
 		}
 	}
 	
 	//View all talks
-	public void displayTalks() {
+	public void displayTalks() 
+	{
 		Event anEvent = selectEvent();
 		
-		if(anEvent != null) {
-			String skip = scanner.nextLine();
-			
+		if(anEvent != null) 
+		{		
 			int count = controller.getNumberOfTalk(anEvent);
 			if(count > 0) {
 				List<Talk> talks = controller.getAllTalk(anEvent);
 				
-				for(int i = 0;i < count;i++) {
+				for(int i = 0;i < count;i++) 
+				{
 					System.out.println(i+1 + ". " + talks.get(i).getTitle() + "\t Speaker: " + talks.get(i).getSpeaker() + "\t Duration: " + talks.get(i).getDuration());
 				}
-			}else {
+			}
+			else 
+			{
 				System.out.println("No talk is found in this event.");
 			}
 			
@@ -190,47 +282,87 @@ public class ConsoleUI {
 		}
 	}
 
-	public Event selectEvent() {
+	public Event selectEvent() 
+	{
 		int choice;
+		String aChoice;		
 		
-		if(controller.getNumberOfEvent()>0) {
+		if(controller.getNumberOfEvent()>0)
+		{
 			displayAllEvent();
 			System.out.print("Please select an event according to their index: ");
-			choice = scanner.nextInt();
+			aChoice = scanner.nextLine();
 			
-			while (choice < 1 || choice > controller.getNumberOfEvent()) {
-	        	System.out.println("Invalid choice.");
-	        	System.out.print("Please enter your choice (1-" + controller.getNumberOfEvent() +"): ");
-	        	choice = scanner.nextInt();
-	    	}
-	
+			boolean wrong = true;
+			do
+			{
+				try
+				{
+					choice = Integer.parseInt(aChoice);
+					while (choice < 1 || choice > controller.getNumberOfEvent()) 
+					{
+			        	System.out.println("Invalid choice.");
+			        	System.out.print("Please enter your choice (1 to " + controller.getNumberOfEvent() +"): ");
+			        	aChoice = scanner.nextLine();
+			        	choice = Integer.parseInt(aChoice);
+			    	}
+					wrong = false;
+				}
+				catch(Exception e)
+				{
+					System.out.print("Please select an event according to their index again correctly: ");
+					aChoice = scanner.nextLine();
+				}
+			}while(wrong == true);
+			
+			choice = Integer.parseInt(aChoice);
 			return controller.selectEvent(choice-1);
-		}else {
+		}
+		else 
+		{
 			System.out.println("No event found.");
 			return null;
 		}
 	}
 	
 	public Guest selectGuest() {
-		int choice;
+		String choice;
+		int aChoice;
 		
 		Event anEvent = displayGuests();
 		
-		if(anEvent != null) {
+		if(anEvent != null && controller.getNumberOfGuest(anEvent)>0) 
+		{
 			System.out.print("Please select guest according to index: ");
-			choice = scanner.nextInt();
-			
-			while (choice < 1 || choice > controller.getNumberOfGuest(anEvent)) {
-	        	System.out.println("Invalid choice.");
-	        	System.out.print("Please enter your choice (1-" + controller.getNumberOfGuest(anEvent) +"): ");
-	        	choice = scanner.nextInt();
-	    	}
-			
-			return controller.selectGuest(choice - 1, anEvent);
-		}else {
+			choice = scanner.nextLine();
+			boolean wrong = true;
+			do
+			{
+				try
+				{
+					aChoice = Integer.parseInt(choice);
+					while (aChoice < 1 || aChoice > controller.getNumberOfGuest(anEvent))
+					{
+			        	System.out.println("Invalid choice.");
+			        	System.out.print("Please enter your choice (1 to " + controller.getNumberOfGuest(anEvent) +"): ");
+			        	choice = scanner.nextLine();
+			        	aChoice = Integer.parseInt(choice);
+			    	}
+					wrong = false;
+				}
+				catch(Exception e)
+				{
+					System.out.println("Invalid input.Please key in again.");
+					choice = scanner.nextLine();
+				}
+			}while(wrong == true);
+		
+			aChoice = Integer.parseInt(choice);
+			return controller.selectGuest(aChoice - 1, anEvent);			
+		}
+		else 
+		{
 			return null;
 		}
 	}
-	
-
 }
